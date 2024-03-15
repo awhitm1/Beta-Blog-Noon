56cc5efa8d4f819d179235539f388c0e812982b2
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-03-14 20:31:29 -0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
-  resources :blogs
+
+  scope '/web' do
+    get 'bootstrap', to: 'web#bootstrap'
+  end
+  
+  resources :blogs do
+    post 'like'
+    delete 'unlike'
+  end
   resources :users, only: [:create]
 end
